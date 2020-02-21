86665f44b3210421831c9c0e4920bc6612085dc1
mzwae <mrmonjaie@gmail.com>
2020-02-21 21:40:38 +0000

    added signup form

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,1 +1,9 @@
-Sign up
+<div class="row">
+  <div class="col-lg-6">
+    <form class="form-vertical" role="form" method="post" action="{{route('auth.signup')}}">
+      <div class="">
+
+      </div>
+    </form>
+  </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,6 @@
 @section('content')
   <h3>Welcome to Social Website</h3>
   <p>A website that will help you connect with your friends</p>
+
+  
 @stop
