f72041917b3565448d0c48838030efef05e5ec7d
mzwae <mrmonjaie@gmail.com>
2020-02-25 21:32:59 +0000

    persist data when error for edit

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -5,7 +5,7 @@
     <form class="form-vertical" role="form" method="post" action="{{route('auth.signup')}}">
       <div class="form-group">
         <label for="email" class="control-label">Your email address</label>
-        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}"  type="email" name="email" id="email" value="">
+        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}"  type="email" name="email" id="email" value="{{ Request::old('email') ?: ''}}">
         @if ($errors->has('email'))
           <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">
             {{ $errors->first('email') }}
@@ -14,14 +14,14 @@
       </div>
       <div class="form-group">
         <label for="username" class="control-label">Choose a username</label>
-        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="text" name="username" value="" id="username">
+        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="text" name="username" value="{{Request::old('username') ?: ''}}" id="username">
         @if ($errors->has('username'))
           <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">{{ $errors->first('username')}}</span>
         @endif
       </div>
       <div class="form-group">
         <label for="password" class="control-label">Choose a password</label>
-        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="password" name="password" value="" id="password">
+        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="password" name="password" value="{{Request::old('password') ?: ''}}" id="password">
         @if ($errors->has('password'))
           <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">{{ $errors->first('password')}}</span>
         @endif
