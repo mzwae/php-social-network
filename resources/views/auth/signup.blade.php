ca57538b4a0a4eec93e58eacf0189a7987d1a3b2
mzwae <mrmonjaie@gmail.com>
2020-02-25 21:39:39 +0000

    organized code

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -4,7 +4,9 @@
   <div class="col-lg-6">
     <form class="form-vertical" role="form" method="post" action="{{route('auth.signup')}}">
       <div class="form-group">
-        <label for="email" class="control-label">Your email address</label>
+        <label for="email" class="control-label">
+          Your email address
+        </label>
         <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}"  type="email" name="email" id="email" value="{{ Request::old('email') ?: ''}}">
         @if ($errors->has('email'))
           <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">
@@ -13,17 +15,25 @@
         @endif
       </div>
       <div class="form-group">
-        <label for="username" class="control-label">Choose a username</label>
-        <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="text" name="username" value="{{Request::old('username') ?: ''}}" id="username">
+        <label for="username" class="control-label">
+          Choose a username
+        </label>
+        <input class="form-control {{$errors->has('email') ? ' is-invalid' : ''}}" type="text" name="username" value="{{Request::old('username') ?: ''}}" id="username">
         @if ($errors->has('username'))
-          <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">{{ $errors->first('username')}}</span>
+          <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">
+            {{$errors->first('username')}}
+          </span>
         @endif
       </div>
       <div class="form-group">
-        <label for="password" class="control-label">Choose a password</label>
+        <label for="password" class="control-label">
+          Choose a password
+        </label>
         <input class="form-control {{ $errors->has('email') ? ' is-invalid' : ''}}" type="password" name="password" value="{{Request::old('password') ?: ''}}" id="password">
         @if ($errors->has('password'))
-          <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">{{ $errors->first('password')}}</span>
+          <span class="help-block {{ $errors->has('email') ? ' text-danger' : ''}}">
+            {{$errors->first('password')}}
+          </span>
         @endif
       </div>
 
