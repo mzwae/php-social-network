11feb291a1997650aac02904ee3144e307e983ad
mzwae <mrmonjaie@gmail.com>
2020-02-29 19:01:11 +0000

    updated navbar

--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -1,35 +1,37 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Social Web</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Social Web</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      @if (Auth::check())
+        <ul class="nav navbar-nav">
+          <li href="#"><a>Timeline</a></li>
+          <li href="#"><a>Friends</a></li>
+        </ul>
+        <form class="navbar-form navbar-left" role="search" acition="#">
+          <div class="form-group">
+            <input type="text" name="query" class="form-control" placeholder="Find people">
+          </div>
+          <button type="sumbit" class="btn btn-default">Search</button>
+        </form>
+      @endif
+      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <ul class="navbar-nav ml-auto">
+        @if (Auth::check())
+          <li><a href="#">Jack{{Auth::user()->getNameOrUsername()}}</a></li>
+          <li><a href="#">Update Profile</a></li>
+          <li><a href="#">Sign Out</a></li>
+        @else
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign in</a>
+          </li>
+        @endif
+      </ul>
+       </div>
+    </div>
   </div>
 </nav>
