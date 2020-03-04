4f6a24b72b4b970ed964969acb4fdcf4020c35ca
mzwae <mrmonjaie@gmail.com>
2020-03-04 09:09:54 +0000

    added action to search form

--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">Social</a>
+    <a class="navbar-brand" href="{{route('home')}}">SocialWeb</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,7 +15,7 @@
                 <a class="nav-link" href="#">Friends</a>
             </li>
 
-            <form class="form-inline my-2 my-lg-0 ml-4">
+            <form class="form-inline my-2 my-lg-0 ml-4" action="{{route('search.results')}}">
                 <input class="form-control mr-sm-2" type="search" placeholder="Find People" aria-label="Search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
