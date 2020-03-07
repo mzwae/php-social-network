c10b8a17c5357d5178ff804683ea870715fa9637
mzwae <mrmonjaie@gmail.com>
2020-03-07 10:20:38 +0000

    added friends list to html template

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,12 +2,20 @@
 
 @section('content')
   <div class="row">
-    <div class="col-lg-5">
+    <div class="col-lg-7">
       @include('user.partials.userblock')
       <hr>
     </div>
-    <div class="col-lg-4 col-lg-offset-3">
+    <div class="col-lg-5 float-right">
+      <h4>{{$user->getNameOrUsername()}}'s friends list:</h4>
 
+      @if (!$user->friends()->count())
+        <p>{{$user->getNameOrUsername()}} has no friends, yet.</p>
+      @else
+        @foreach ($user->friends() as $user)
+          @include('user/partials/userblock')
+        @endforeach
+      @endif
     </div>
   </div>
 @endsection
