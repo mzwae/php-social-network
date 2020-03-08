5d7f945d5d554e4dfac274b2561e6271fea84386
mzwae <mrmonjaie@gmail.com>
2020-03-08 09:25:55 +0000

    added friends view

---
+++ resources/views/friends/index.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.default')
+@section('content')
+<div class="row">
+    <div class="col-lg-6">
+        <h3>Your Friends</h3>
+    </div>
+    <div class="col-lg-6">
+        Friend Requests
+    </div>
+</div>
+@endsection
