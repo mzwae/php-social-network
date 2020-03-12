c1738da24e076c6593556b36dbaa1c59569051f9
mzwae <mrmonjaie@gmail.com>
2020-03-12 18:58:26 +0000

    added session token

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -55,11 +55,12 @@
                     </div> --}}
 
 
-                    <form role="form" action="#" method="post">
+                    <form role="form" action="{{route('status.reply')}}" method="post">
                         <div class="form-group">
-                            <textarea name="reply-1" class="form-control" rows="2" placeholder="Reply to this status"></textarea>
+                            <textarea name="reply-{{$status->id}}" class="form-control" rows="2" placeholder="Reply to this status"></textarea>
                         </div>
                         <input type="submit" value="Reply" class="btn btn-default btn-sm">
+                        <input type="hidden" name="_token" value="{{Session::token()}}">
                     </form>
                 </div>
             </div>
