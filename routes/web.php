08b2c9fa98e9e9eaac03d38e91c7661c38fe6185
mzwae <mrmonjaie@gmail.com>
2020-03-14 09:46:51 +0000

    added like route

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,11 @@ Route::post('/status/{statusId}/reply', [
   'as' => 'status.reply',
   'middleware' => ['auth'],
 ]);
+Route::get('/status/{statusId}/like', [
+  'uses' => '\App\Http\Controllers\StatusController@getLike',
+  'as' => 'status.like',
+  'middleware' => ['auth'],
+]);
 
 
 
