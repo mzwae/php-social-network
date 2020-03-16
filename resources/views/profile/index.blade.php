cf09fbd9dd4c8a23136d37a8a231584b07a56603
mzwae <mrmonjaie@gmail.com>
2020-03-16 13:33:23 +0000

    retrieved accept friend button in profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,11 +2,33 @@
 
 @section('content')
 
-  <div class="row">
-    <div class="col-lg-7">
-      @include('user.partials.userblock')
-    </div>
-  </div>
+      <div class="row">
+        <div class="col-lg-7">
+          @include('user.partials.userblock')
+        </div>
+        <div class="col-lg-5 float-right">
+          @if (Auth::user()->hasFriendRequestPending($user))
+            <p>Waiting for {{$user->getNameOrUsername()}} to accept your request.</p>
+          @elseif (Auth::user()->hasFriendRequestReceived($user))
+            <a href="{{route('friends.accept', ['username' => $user->username])}}" class="btn btn-outline-success my-2 my-sm-0">Accept Friend Request</a>
+
+          @elseif (Auth::user()->isFriendsWith($user))
+            <p class="text-success">You and {{$user->getNameOrUsername()}} are friends.</p>
+          @else
+            <a href="{{route('friends.add', ['username' => $user->username])}}" class="btn btn-outline-success my-2 my-sm-0">Add as friend</a>
+          @endif
+
+          <h4>{{$user->getNameOrUsername()}}'s friends list:</h4>
+
+          @if (!$user->friends()->count())
+            <p>{{$user->getNameOrUsername()}} has no friends, yet.</p>
+          @else
+            @foreach ($user->friends() as $user)
+              @include('user/partials/userblock')
+            @endforeach
+          @endif
+        </div>
+      </div>
       <hr>
       <div class="row">
         <div class="col-lg-5">
