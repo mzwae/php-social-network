2e857e054dedcf9a2db381cc7e8e021847115224
mzwae <mrmonjaie@gmail.com>
2020-03-23 09:20:53 +0000

    added about nav tab

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,5 +5,5 @@
   <p>A website that will help you connect with your friends and make new friends.</p>
   <p>Your profile image will be pulled automatically from <a href="https://en.gravatar.com/">gravatar website</a>, using your login email.</p>
   <p>I hope you enjoy using it, and find it useful.</p>
-  <h4>Thanks.</h4>
+  <h5>Thanks &#128150;</h5>
 @stop
--- resources/views/templates/partials/navigation.blade.php
+++ resources/views/templates/partials/navigation.blade.php
@@ -8,7 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             @if (Auth::check())
-            <li class="nav-item active">
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('about')}}">About</a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="{{route('home')}}">Timeline</a>
             </li>
             <li class="nav-item">
