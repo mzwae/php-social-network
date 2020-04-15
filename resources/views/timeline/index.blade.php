9cdd3ec0e946e5c39d4539d728097f93f0c0c91c
mzwae <mrmonjaie@gmail.com>
2020-04-15 02:17:56 +0100

    Add edit reply modal

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -134,6 +134,36 @@
                         </ul>
                     </div>
                 </div>
+
+                      <!-- The edit reply Modal -->
+                      <div class="modal" id="editModal-{{$reply->id}}">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+  
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                              <h4 class="modal-title">Edit Your Reply</h4>
+                              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+  
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                            <form action="{{route('status.edit', ['statusId'=>$reply->id])}}" method="post">
+                                <div class="form-group">
+                                  <label for="status-body">Edit Reply</label>
+                                  <textarea class="form-control" name="status-body" id="reply-body" cols="30" rows="10">{{$reply->body}}</textarea>
+                                </div>
+  
+                                <input type="submit" value="Save" class="btn btn-outline-primary">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                                <input type="hidden" name="_token" value="{{Session::token()}}">
+                              </form>
+                            </div>
+  
+  
+                          </div>
+                        </div>
+                      </div>
               @endforeach
 
 
