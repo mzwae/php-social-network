ec7a4f9c9cdcc9f7fb66ea59c13e99826f60d9b0
mzwae <mrmonjaie@gmail.com>
2020-04-18 01:56:52 +0100

    Reorder bootstrap theme file

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -10,9 +10,9 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{route('home')}}\css\bootstrap.min7.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="{{route('home')}}\css\bootstrap.min7.css">
     <link rel="stylesheet" href="{{route('home')}}\css\modal-styles.css">
     <style media="screen">
     
