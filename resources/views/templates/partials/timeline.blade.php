660e57949e8d37b59863e52ed5ad24600a37e26f
mazwae <mrmonjaie@gmail.com>
2020-04-30 22:15:22 +0100

    Include edit status modal template

--- resources/views/templates/partials/timeline.blade.php
+++ resources/views/templates/partials/timeline.blade.php
@@ -160,35 +160,8 @@
             </div>
             <hr>
 
-                                <!-- The edit status Modal -->
-                <div class="modal" id="editModal-{{$status->id}}">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-
-                      <!-- Modal Header -->
-                      <div class="modal-header">
-                        <h4 class="modal-title">Edit Your Status</h4>
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                      </div>
-
-                      <!-- Modal body -->
-                      <div class="modal-body">
-                      <form action="{{route('status.edit', ['statusId'=>$status->id])}}" method="post">
-                          <div class="form-group">
-                            <label for="status-body">Edit Status</label>
-                            <textarea class="form-control" name="status-body" id="status-body" cols="30" rows="10">{{$status->body}}</textarea>
-                          </div>
-
-                          <input type="submit" value="Save" class="btn btn-outline-primary">
-                          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-                          <input type="hidden" name="_token" value="{{Session::token()}}">
-                        </form>
-                      </div>
-
-
-                    </div>
-                  </div>
-                </div>
+                  <!-- The edit status Modal -->
+              @include('templates.partials.editmodal', ['id' => $status->id, 'title' => 'status', 'body' => $status->body])
 
                           <!-- Delete Status Button Modal HTML -->
               @include('templates.partials.deletemodal', ['id' => $status->id])
